f46ce93904d1a64546218cdcbf44512a900fef55
rossyKasteleria <rossy.kasteleria@gmail.com>
2023-12-19 09:11:17 -0300

    Estudo de colunas e linhas

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,41 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              )
+            ],
+          )
+        )
       ),
     );
   }
